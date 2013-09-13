026b8a70fb9ade4f1c289db24ed6f6c57ca68106
Nico Preston <nicopresto@gmail.com>
2013-09-13 11:04:51 -0400

    adjusted indentation

--- modules/people/manifests/nicopresto.pp
+++ modules/people/manifests/nicopresto.pp
@@ -1,10 +1,13 @@
 class people::nicopresto {
+  include libreoffice
+  
   git::config::global {
     'user.name':
       value => 'Nico Preston';
     'user.email':
       value => 'nicopresto@gmail.com';
-    $home     = "/Users/${::boxen_user}"
   }
+  $home     = "/Users/${::boxen_user}"
   include projects::all
+
 }
