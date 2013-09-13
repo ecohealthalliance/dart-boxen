a79745dd37f68a5ba8ad7219ed253262b9a7881d
Nico Preston <nicopresto@gmail.com>
2013-09-13 12:45:40 -0400

    added aliases to nicopresto

--- modules/people/manifests/nicopresto.pp
+++ modules/people/manifests/nicopresto.pp
@@ -5,6 +5,12 @@ class people::nicopresto {
       value => 'Nico Preston';
     'user.email':
       value => 'nicopresto@gmail.com';
+     'alias.st':  value => 'status';
+     'alias.ci':  value => 'commit';
+     'alias.co':  value => 'checkout';
+     'alias.di':  value => 'diff';
+     'alias.dc':  value => 'log -p';
+     'color.ui':  value => 'true';
   }
   $home     = "/Users/${::boxen_user}"
   include projects::all
