853c5b0e53364957fb032dac6d1ce43a2e819842
Amy Slagle <slagle@ehatek.org>
2013-09-17 12:08:46 -0400

    Run npm install and grunt in grits client on project setup.

--- modules/projects/manifests/grits.pp
+++ modules/projects/manifests/grits.pp
@@ -1,6 +1,7 @@
 class projects::grits {
   boxen::project { 'grits':
     source => 'ecohealthalliance/grits',
+    notify => Exec['grits client setup'];
   }
 
   python::mkvirtualenv { 'grits':
@@ -10,5 +11,12 @@ class projects::grits {
   python::requirements { 'grits':
     requirements => "${boxen::config::srcdir}/grits/server/requirements.txt",
     virtualenv   => 'grits',
+  }
+
+  exec { 'grits client setup':
+    cwd => "${boxen::config::srcdir}/grits/client",
+    path => "/bin:/usr/bin:${boxen::config::home}/nodenv/shims:${boxen::config::home}/nodenv/versions/v0.10/bin",
+    command => "npm install; grunt";
+
   }
 }
