5cd06cb95fbfbd42cbd8c7cc91efe2d9911c32fa
Amy Slagle <slagle@ehatek.org>
2013-09-17 11:48:40 -0400

    Add additional finder settings.

--- modules/eha/manifests/osx_config.pp
+++ modules/eha/manifests/osx_config.pp
@@ -5,12 +5,79 @@ class eha::osx_config {
   include osx::disable_app_quarantine
   include osx::software_update
   include osx::finder
-  boxen::osx_defaults { 'Show all files':
-    user   => $::boxen_user,
-    key    => 'AppleShowAllFiles',
-    domain => 'com.apple.finder',
-    value  => true,
-    notify => Exec['killall Finder'];
+
+  boxen::osx_defaults {
+    'Show all files':
+      ensure => present,
+      key    => 'AppleShowAllFiles',
+      domain => 'com.apple.finder',
+      value  => true,
+      user   => $::boxen_user,
+      notify => Exec['killall Finder'];
+  
+    'Show all filename extensions':
+      ensure => present,
+      key    => 'AppleShowAllExtensions',
+      domain => 'NSGlobalDomain',
+      value  => 'true',
+      type   => 'bool',
+      user   => $::boxen_user;
+
+    'Show status bar':
+      ensure => present,
+      key    => 'ShowStatusBar',
+      domain => 'com.apple.finder',
+      value  => 'true',
+      type   => 'bool',
+      user   => $::boxen_user;
+
+    'Allow text selection in Quick Look':
+      ensure => present,
+      key    => 'QLEnableTextSelection',
+      domain => 'com.apple.finder',
+      value  => 'true',
+      type   => 'bool',
+      user   => $::boxen_user;
+
+    'Display full POSIX path as Finder window title':
+      ensure => present,
+      key    => '_FXShowPosixPathInTitle',
+      domain => 'com.apple.finder',
+      value  => 'true',
+      type   => 'bool',
+      user   => $::boxen_user;
+
+    'Disable the warning when changing a file extension':
+      ensure => present,
+      key    => 'FXEnableExtensionChangeWarning',
+      domain => 'com.apple.finder',
+      value  => 'false',
+      type   => 'bool',
+      user   => $::boxen_user;
+
+    'Avoid creating .DS_Store files on network volumes':
+      ensure => present,
+      key    => 'DSDontWriteNetworkStores',
+      domain => 'com.apple.desktopservices',
+      value  => 'true',
+      type   => 'bool',
+      user   => $::boxen_user;
+
+   'Use list view in all Finder windows by default':
+      ensure => present,
+      key    => 'FXPreferredViewStyle',
+      domain => 'com.apple.finder',
+      value  => 'Nlsv',
+      type   => 'string',
+      user   => $::boxen_user;
+
+    'Disable the warning before emptying the Trash':
+      ensure => present,
+      key    => 'com.apple.finder',
+      domain => 'WarnOnEmptyTrash',
+      value  => 'false',
+      type   => 'bool',
+      user   => $::boxen_user;
   }
 
   dockutil::item {
